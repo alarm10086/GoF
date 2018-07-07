204daefe0bf71639d3ec1ba2bb2905b10c09c6af
alarm10086 <alarm10086@gmail.com>
2018-07-08 02:22:24 +0800

    为每个设计模式补充uml类图

--- src/main/java/iterator/sample/BookShelf.java
+++ src/main/java/iterator/sample/BookShelf.java
@@ -3,14 +3,14 @@ package iterator.sample;
 import java.util.ArrayList;
 
 public class BookShelf implements Aggregate<Book> {
-    private final ArrayList books;
+    private final ArrayList<Book> books;
 
     public BookShelf(final int initialSize) {
         this.books = new ArrayList(initialSize);
     }
 
     public Book getBookAt(final int index) {
-        return (Book) books.get(index);
+        return books.get(index);
     }
 
     public void appendBook(final Book book) {
